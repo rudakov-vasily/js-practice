ddcbf62628a60593f0d6344ba6675c7790760100
rudakov-vasily <rudakov_v_v@mail.ru>
2023-07-24 23:56:59 +0300

    continue to learning JS

--- async/script.js
+++ async/script.js
@@ -13,7 +13,7 @@ const displayCountry = function (data, className = '') {
   const languages = data.languages;
   const firstLanguage = Object.values(languages)[0];
 
-  const html = `        
+  const html = `
   <article class="country ${className}">
   <img class="country__img" src="${data.flags.svg}" />
   <div class="country__data">
@@ -28,35 +28,49 @@ const displayCountry = function (data, className = '') {
   countriesContainer.style.opacity = 1;
 };
 
-//вызов AJAX для получения данных о стране
-const getCountryAndBorderCountries = function (countryName) {
-  const request1 = new XMLHttpRequest();
-  request1.open('GET', `https://restcountries.com/v3.1/name/${countryName}`);
-  request1.send();
-  request1.addEventListener('load', function () {
-    const [data1] = JSON.parse(this.responseText);
-    console.log(data1);
-    //отображение страны
-    displayCountry(data1);
+// //вызов AJAX для получения данных о стране
+// const getCountryAndBorderCountries = function (countryName) {
+//   const request1 = new XMLHttpRequest();
+//   request1.open('GET', `https://restcountries.com/v3.1/name/${countryName}`);
+//   request1.send();
+//   request1.addEventListener('load', function () {
+//     const [data1] = JSON.parse(this.responseText);
+//     console.log(data1);
+//     //отображение страны
+//     displayCountry(data1);
 
-    //получаем соседние страны, чначала первую
-    const [firstNEigbour] = data1.borders;
-    if (!firstNEigbour) return;
+//     //получаем соседние страны, чначала первую
+//     const [firstNEigbour] = data1.borders;
+//     if (!firstNEigbour) return;
 
-    //вызов AJAX для получения данных о соседней стране
-    const request2 = new XMLHttpRequest();
-    request2.open(
-      'GET',
-      `https://restcountries.com/v3.1/alpha/${firstNEigbour}`
-    );
-    request2.send();
+//     //вызов AJAX для получения данных о соседней стране
+//     const request2 = new XMLHttpRequest();
+//     request2.open(
+//       'GET',
+//       `https://restcountries.com/v3.1/alpha/${firstNEigbour}`
+//     );
+//     request2.send();
 
-    request2.addEventListener('load', function () {
-      const [data2] = JSON.parse(this.responseText);
-      console.log(data2);
-      displayCountry(data2, 'neighbour');
-    });
-  });
+//     request2.addEventListener('load', function () {
+//       const [data2] = JSON.parse(this.responseText);
+//       console.log(data2);
+//       displayCountry(data2, 'neighbour');
+//     });
+//   });
+// };
+
+// getCountryAndBorderCountries('belarus');
+
+// const getCountryAndBorderCountries = function (countryName) {
+//   const request1 = new XMLHttpRequest();
+//   request1.open('GET', `https://restcountries.com/v3.1/name/${countryName}`);
+//   request1.send();
+
+const request = fetch('https://restcountries.com/v3.1/name/russia');
+
+const getCountryData = function (countryName) {
+  fetch(`https://restcountries.com/v3.1/name/${countryName}`)
+    .then((response) => response.json())
+    .then((data) => displayCountry(data[0]));
 };
-
-getCountryAndBorderCountries('belarus');
+getCountryData('russia');

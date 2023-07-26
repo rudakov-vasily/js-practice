c9e9eab7f76d8002cea9fc4e4d16b4d0793a63a3
rudakov-vasily <rudakov_v_v@mail.ru>
2023-07-26 22:41:21 +0300

    continue to learning JS

--- async/script.js
+++ async/script.js
@@ -68,39 +68,123 @@ const displayCountry = function (data, className = '') {
 
 // const request = fetch('https://restcountries.com/v3.1/name/russia');
 
-const getCountryData = function (countryName) {
-  fetch(`https://restcountries.com/v3.1/name/${countryName}`)
-    .then((response) => response.json())
-    .then((data) => {
-      displayCountry(data[0]);
-      const firstNeighbour = data[0].borders[0];
+// const getCountryData = function (countryName) {
+//   fetch(`https://restcountries.com/v3.1/name/${countryName}`)
+//     .then((response) => response.json())
+//     .then((data) => {
+//       displayCountry(data[0]);
+//       const firstNeighbour = data[0].borders[0];
 
-      if (!firstNeighbour) return;
+//       if (!firstNeighbour) return;
 
-      return fetch(`https://restcountries.com/v3.1/alpha/${firstNeighbour}`);
-    })
-    .then((response) => response.json())
-    .then((data) => displayCountry(data[0], 'neighbour'))
-    .catch((e) => console.log(`somebody catch${e}`))
-    .finally(() => {
-      countriesContainer.style.opacity = 1;
-    });
-};
+//       return fetch(`https://restcountries.com/v3.1/alpha/${firstNeighbour}`);
+//     })
+//     .then((response) => response.json())
+//     .then((data) => displayCountry(data[0], 'neighbour'))
+//     .catch((e) => console.log(`somebody catch${e}`))
+//     .finally(() => {
+//       countriesContainer.style.opacity = 1;
+//     });
+// };
 
 // btn.addEventListener('click', function () {
 //   getCountryData('russia');
 // });
 
-const displayCountryByGPS = function (lat, lng) {
-  fetch(`https://geocode.maps.co/reverse?lat=${lat}&lon=${lng}`)
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(`you are in ${data.address.country}`);
+// const displayCountryByGPS = function (lat, lng) {
+//   fetch(`https://geocode.maps.co/reverse?lat=${lat}&lon=${lng}`)
+//     .then((response) => response.json())
+//     .then((data) => {
+//       console.log(`you are in ${data.address.country}`);
 
-      getCountryData(`${data.address.country}`);
-    });
-};
+//       getCountryData(`${data.address.country}`);
+//     });
+// };
 
 // displayCountryByGPS(35.756, 139.256);
-displayCountryByGPS(48.857, 2.358);
-displayCountryByGPS(40.708, -74.051);
+// displayCountryByGPS(48.857, 2.358);
+// displayCountryByGPS(40.708, -74.051);
+
+// const lotteryPromise = new Promise(function (resolve, reject) {
+//   console.log('this is a magic!...');
+//   setTimeout(function () {
+//     if (Math.random() >= 0.5) {
+//       resolve('you are a winner!');
+//     } else {
+//       reject(new Error('you are a looser!...'));
+//     }
+//   }, 3000);
+// });
+
+// lotteryPromise
+//   .then((res) => console.log(res))
+//   .catch((e) => console.error(e))
+//   .finally(() => console.log('END'));
+
+// const imageContainer = document.querySelector('.images');
+// console.log(imageContainer);
+
+// const wait = function (second) {
+//   return new Promise(function (resolve) {
+//     setTimeout(resolve, second * 1000);
+//   });
+// };
+
+// let currentImage;
+
+// const createImageElement = function (imagePath) {
+//   return new Promise(function (resolve, reject) {
+//     const imgEl = document.createElement('img');
+//     imgEl.src = imagePath;
+
+//     imgEl.addEventListener('load', function () {
+//       imageContainer.append(imgEl);
+//       resolve(imgEl);
+//     });
+
+//     imgEl.addEventListener('error', function () {
+//       reject(new Error('img not found'));
+//     });
+//   });
+// };
+
+// createImageElement('img/image1.jpg')
+//   .then((imgEl) => {
+//     currentImage = imgEl;
+//     console.log('1 img load');
+//     return wait(2);
+//   })
+//   .then(() => {
+//     currentImage.style.display = 'none';
+//     return createImageElement('img/image2.jpg');
+//   })
+//   .then((imgEl) => {
+//     currentImage = imgEl;
+//     console.log('2 img load');
+//     return wait(2);
+//   })
+//   .then(() => {
+//     currentImage.style.display = 'none';
+//     return createImageElement('img/image3.jpg');
+//   })
+//   .then((imgEl) => {
+//     currentImage = imgEl;
+//     console.log('3 img load');
+//     return wait(2);
+//   })
+//   .then(() => {
+//     currentImage.style.display = 'none';
+//   })
+//   .catch((e) => console.error(e));
+
+const getCountryData = async function (countryName) {
+  const response = await fetch(
+    `https://restcountries.com/v3.1/name/${countryName}`
+  );
+
+  const data = await response.json();
+  console.log(data[0]);
+  displayCountry(data[0]);
+};
+
+getCountryData('russia');
